19bc74b1a365c5e1a89ecbd523da969fc44411a4
zhangyuge666 <2546092154@qq.com>
2025-11-01 15:13:21 +0800

    first commit

---
+++ .vitepress/theme/components/ShareCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="shareCard">
+    <h1 class="title">
+      <span>向风车发起冲锋 🏹</span>
+    </h1>
+    <p class="description">
+      天还没亮，继续走吧
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+<style scoped>
+.shareCard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 15px;
+  text-align: center;
+}
+.title {
+  display: flex;
+  align-items: center;
+  font-size: 2.2em;
+}
+.description {
+  margin: 1em 0;
+}
+</style>
